ad330bdaecdb55519b2e4136fc3f51a8f8ed86f5
zahidmiran <zahidmiron599@gmail.com>
2022-05-23 01:30:27 +0600

    My Orders Page Added

--- src/components/pages/Dashboard/User/MyOrders.js
+++ src/components/pages/Dashboard/User/MyOrders.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../../firebase.init';
+import { userColumns, userRows } from '../User/dataSource'
+
+import './MyOrders.scss'
 
 const MyOrders = () => {
+    const [data, setData] = useState(userRows);
+
+    const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    const handleDelete = (id) => {
+        setData(data.filter((item) => item.id !== id));
+    };
+
+    const actionColumn = [
+        {
+            field: "action",
+            headerName: "Action",
+              width: 200,
+            renderCell: (params) => {
+                return (
+                    <div className="cellAction">
+                        <Link to="/users/test" style={{ textDecoration: "none" }}>
+                            <div className="viewButton">View</div>
+                        </Link>
+                        <div
+                            className="deleteButton"
+                            onClick={() => handleDelete(params.row.id)}
+                        >
+                            Delete
+                        </div>
+                    </div>
+                );
+            },
+        },
+    ];
+
     return (
-        <div>
-            My Orders
-            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos beatae totam similique, et voluptatibus deleniti, placeat aperiam inventore, ex ad voluptatum omnis? Vel a explicabo dolores eligendi perferendis voluptatibus itaque!</h1>
+        <div className="datatable">
+            <div className="datatableTitle">
+                Add New User
+                <Link to="/users/new" className="link">
+                    Add New
+                </Link>
+            </div>
+            <DataGrid
+                className="datagrid"
+                rows={data}
+                columns={userColumns.concat(actionColumn)}
+                pageSize={9}
+                rowsPerPageOptions={[9]}
+                checkboxSelection
+            />
         </div>
     );
 };
